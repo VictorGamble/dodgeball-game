69012c39cd4989c6f50d15327f01367392dd7311
Victor <Victor.Gamble@outlook.com>
2020-01-24 15:30:52 -0500

    opposition class

---
+++ opposition_clas.py
@@ -0,0 +1,14 @@
+import pygame
+from player_class import *
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+class Opposition(Player):
+    pass
