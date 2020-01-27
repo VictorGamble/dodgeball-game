f7f64606b5aeb6f66bd747af930cc17f761aff7c
Victor <Victor.Gamble@outlook.com>
2020-01-26 19:30:57 -0500

    this is the class i am having problems with

--- Ball_Clas.py
+++ Ball_Clas.py
@@ -2,10 +2,21 @@
 
  
 #imports
-import pygame
+import pygame 
 import random
 from os import path
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+    K_SPACE
+)
 
+keys = pygame.key.get_pressed()
 img_dir = path.join(path.dirname(__file__), 'img')
 ball_img = pygame.image.load(path.join(img_dir, "sphere-11.png"))
 
@@ -38,44 +49,60 @@ class Ball(pygame.sprite.Sprite):
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
     
         
+            
     def update(self):
-        # any code here will happen every time the game loop updates
-        self.rect.x -= 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
-        if self.rect.left < 0:
-            self.kill()
-        elif self.rect.right < 0:
-            self.kill()
-
-
-
+            # any code here will happen every time the game loop updates
+        if keys[K_UP]:
+                self.rect.move_ip(0, -5)
+      
 
 
 class Player(pygame.sprite.Sprite):
-    
-
+     # sprite for the Player
     def __init__(self):
-         # this line is required to properly create the sprite
+            # this line is required to properly create the sprite
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
+            # create a plain rectangle for the sprite image
+        self.image = pygame.transform.scale(ball_img, (25,25))
+
+            # find the rectangle that encloses the image
         self.rect = self.image.get_rect()
-        #draw a circle
+            #draw a circle
         self.radius = int(self.rect.width * .85/ 2)
-        #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
-        # center the sprite on the screen
+            #pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
+            # center the sprite on the screen
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
     
-        
     def update(self):
-        # any code here will happen every time the game loop updates
-        self.rect.x -= 0
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
-        # if self.rect.left < 0:
-        #     self.kill()
-        # elif self.rect.right < 0:
-        #     self.kill()
+
+        def update(self, pressed_keys):
+            SCREEN_WIDTH = 800
+            SCREEN_HEIGHT = 600
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0, -5)
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0, 5)
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+            if self.rect.left < 0:
+                self.rect.left = 0
+            if self.rect.right > SCREEN_WIDTH:
+                self.rect.right = SCREEN_WIDTH
+            if self.rect.top <= 0:
+                self.rect.top = 0
+            if self.rect.bottom >= SCREEN_HEIGHT:
+                self.rect.bottom = SCREEN_HEIGHT
+
+
+
+
+
+
+        
 
         
 
@@ -96,7 +123,6 @@ ball1 = Ball()
 ball2 = Ball()
 player = Player()
 all_sprites.add(ball1)
-all_sprites.add(ball2)
 all_sprites.add(player)
 # Game loop
 running = True
@@ -114,7 +140,7 @@ while running:
     # Update
     all_sprites.update()
 
-    pygame.sprite.spritecollide(Player, Ball, True)
+    # pygame.sprite.spritecollide(Player, Ball, True)
     
 
     
@@ -132,7 +158,3 @@ while running:
 # #player shooting fuct
 # def shoot(self):
 #     ball1 = Ball(self.center.centerx)
-
-
-
-        
